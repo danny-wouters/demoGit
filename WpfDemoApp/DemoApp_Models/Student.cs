83faaf1eb075c0bf239c1b60d38ff969c680abfc
Danny Wouters <danny_wouters9@hotmail.com>
2020-04-21 09:24:02 +0200

    Add Demo application

---
+++ WpfDemoApp/DemoApp_Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp_Models
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
