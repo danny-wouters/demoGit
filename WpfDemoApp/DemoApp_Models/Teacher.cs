f63ad5a05cf660a3644cd824ba701ed7c5069975
Danny Wouters <danny_wouters9@hotmail.com>
2020-04-24 10:48:36 +0200

    Update and add fields to teacher model

--- WpfDemoApp/DemoApp_Models/Teacher.cs
+++ WpfDemoApp/DemoApp_Models/Teacher.cs
@@ -7,9 +7,10 @@ namespace DemoApp_Models
     class Teacher
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
         public string Address { get; set; }
         public string Telephonenumber { get; set; }
+        public string City { get; set; }
     }
 }
