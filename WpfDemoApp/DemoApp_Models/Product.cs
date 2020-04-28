204104f8aa84b52f1778863359d484b95fb25a76
Danny Wouters <danny_wouters9@hotmail.com>
2020-04-28 10:54:51 +0200

    Add type and price to product

--- WpfDemoApp/DemoApp_Models/Product.cs
+++ WpfDemoApp/DemoApp_Models/Product.cs
@@ -9,5 +9,7 @@ namespace DemoApp_Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
     }
 }
