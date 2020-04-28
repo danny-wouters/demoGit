fe19402d2e69ba4fb205ce00c4468495c7ba8e20
Danny Wouters <danny_wouters9@hotmail.com>
2020-04-28 09:47:33 +0200

    Add Code to Course model

--- WpfDemoApp/DemoApp_Models/Course.cs
+++ WpfDemoApp/DemoApp_Models/Course.cs
@@ -7,6 +7,7 @@ namespace DemoApp_Models
     class Course
     {
         public int Id { get; set; }
+        public string Code { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string FullDescription { get; set; }
