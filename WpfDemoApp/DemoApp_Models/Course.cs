0b3012cea1efdbcfeff8d624236b6b25645b8fb6
Danny Wouters <danny_wouters9@hotmail.com>
2020-04-28 09:25:07 +0200

    Add duration to Course + Add StudentCourse model

--- WpfDemoApp/DemoApp_Models/Course.cs
+++ WpfDemoApp/DemoApp_Models/Course.cs
@@ -10,5 +10,6 @@ namespace DemoApp_Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string FullDescription { get; set; }
+        public int Duration { get; set; }
     }
 }
