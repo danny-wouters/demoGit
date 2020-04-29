b7d9fb13b048601b1dcbed45acfc1946fd38d6b7
Danny Wouters <danny_wouters9@hotmail.com>
2020-04-29 11:15:17 +0200

    Rename Description to ShortDescription

--- WpfDemoApp/DemoApp_Models/Product.cs
+++ WpfDemoApp/DemoApp_Models/Product.cs
@@ -8,7 +8,7 @@ namespace DemoApp_Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string ShortDescription { get; set; }
         public double Price { get; set; }
     }
 }
