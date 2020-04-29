4f52bfa7277c36107088bb9e9951b2da0aa2d75d
Danny Wouters <danny_wouters9@hotmail.com>
2020-04-29 09:40:24 +0200

    Add book model + remove type property from product model

---
+++ WpfDemoApp/DemoApp_Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp_Models
+{
+    class Book: Product
+    {
+        public string Author { get; set; }
+    }
+}
--- WpfDemoApp/DemoApp_Models/Product.cs
+++ WpfDemoApp/DemoApp_Models/Product.cs
@@ -9,7 +9,6 @@ namespace DemoApp_Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Type { get; set; }
         public double Price { get; set; }
     }
 }
